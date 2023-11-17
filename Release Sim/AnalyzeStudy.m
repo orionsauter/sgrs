8bdbe250cdb9b4a73561138d91ed3ebf6950494b
Orion Sauter <orion.sauter@ligo.org>
2023-11-17 08:25:10 -0500

    Plot all outputs in a PDF

--- Release Sim/AnalyzeStudy.m
+++ Release Sim/AnalyzeStudy.m
@@ -1,19 +1,101 @@
-function AnalyzeStudy(study_name, xlab)
+function AnalyzeStudy(study_name, xlab, varargin)
 params = readtable(strcat(study_name,'/params.txt'));
-% figure(1);
-% hold on;
-y_vels = zeros([size(params,1),1]);
+if size(params,2) > 2
+    param_values = params.(strcat('param_values_',string(varargin{1})));
+else
+    param_values = params.param_values;
+end
 for i=params.idx'
     matfile = dir(strcat(study_name,'/',study_name,string(i),'_*/TMrel_*.mat'));
-    load(strcat(matfile.folder,'/',matfile.name), 't','vTM');
-    y_vels(i) = max(abs(vTM(:,2)));
-    % plot(t, vTM(:,2), "-");
+    data(i) = load(strcat(matfile.folder,'/',matfile.name));
 end
-% legend(string(params.param_values'));
-% xlabel('time [s]');
-% ylabel('TM y vel. [m/s]');
-plot(params.param_values, y_vels, ".-");
+
+figure;
+plot(param_values, arrayfun(@(x) max(abs(x.xTM(:,1))), data), '.-');
 xlabel(xlab);
-ylabel('max y vel. [m/s]');
-saveas(figure(1),strcat(study_name,'/y_vel.png'));
+ylabel('max x disp. [m]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",false);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) max(abs(x.xTM(:,2))), data), '.-');
+xlabel(xlab);
+ylabel('max y disp. [m]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) max(abs(x.xTM(:,3))), data), '.-');
+xlabel(xlab);
+ylabel('max z disp. [m]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) max(abs(x.bTM(:,1))), data), '.-');
+xlabel(xlab);
+ylabel('max psi disp. [rad]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) max(abs(x.bTM(:,2))), data), '.-');
+xlabel(xlab);
+ylabel('max theta disp. [rad]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) max(abs(x.bTM(:,3))), data), '.-');
+xlabel(xlab);
+ylabel('max phi disp. [rad]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) x.vTM(end,1), data), '.-');
+xlabel(xlab);
+ylabel('final x vel. [m/s]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) x.vTM(end,2), data), '.-');
+hold on;
+if max(arrayfun(@(x) x.vTM(end,2), data))/4.5e-6 > 0.1
+    yline(4.5e-6, '--r');     % Max residual velocity from "ResVel_2DOF.m"
+    legend('', 'limit');
+end
+xlabel(xlab);
+ylabel('final y vel. [m/s]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) x.vTM(end,3), data), '.-');
+xlabel(xlab);
+ylabel('final z vel. [m/s]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) x.wTM(end,1), data), '.-');
+xlabel(xlab);
+ylabel('final psi vel. [rad/s]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) x.wTM(end,2), data), '.-');
+xlabel(xlab);
+ylabel('final theta vel. [rad/s]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
+
+figure;
+plot(param_values, arrayfun(@(x) x.wTM(end,3), data), '.-');
+xlabel(xlab);
+ylabel('final phi vel. [rad/s]');
+exportgraphics(gcf,strcat(study_name,'/Results.pdf'),"Append",true);
+close;
 end
